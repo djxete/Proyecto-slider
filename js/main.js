e2daeb014d4849fca6b98bdb2a01438056bea55d
Ignacio Herráez <ignacio_hg_87@hotmail.com>
2020-12-22 10:10:05 +0100

    proyecto terminado

--- js/main.js
+++ js/main.js
@@ -1,1 +1,62 @@
-alert("probando");
+//alert("probando");
+
+// ============= VARIABLES ===================
+const slides= document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".nextBtn");
+const prevBtn = document.querySelector(".prevBtn");
+
+
+//Añadimos la propiedad left a cada slide
+
+slides.forEach((slide,index) => {
+
+    slide.style.left = `${index * 100}%`;
+    
+})
+
+
+let counter = 0;
+
+
+nextBtn.addEventListener("click", ()=>{
+    counter++;
+    pasarSlide();
+});
+
+prevBtn.addEventListener("click", ()=>{
+    counter--;
+    pasarSlide();
+
+});
+
+
+
+
+function pasarSlide(){
+
+    // trabajando con los botones
+
+    if(counter < slides.length - 1){
+        nextBtn.style.display = "block";
+    } 
+
+    if(counter === slides.length - 1){
+        nextBtn.style.display = "none";
+    }
+
+    if(counter > 0){
+        prevBtn.style.display = "block";
+    }
+    else {
+        prevBtn.style.display = "none";
+    }
+
+    slides.forEach(slide =>{
+        slide.style.transform = `translateX(-${counter * 100}%)`;
+    })
+}
+
+
+prevBtn.style.display = "none";
+
+
